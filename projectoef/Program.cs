e4671b8b61067294701646cbf6f7a46c0211b597
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2023-09-21 11:52:09 -0500

    Cambio de conexion para creacion a base de datos postgres, adicional, se utiliza connectionString

--- projectoef/Program.cs
+++ projectoef/Program.cs
@@ -1,13 +1,19 @@
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using projectoef;
+using System.Xml.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
+// configuracion de base de datos en en memoria
+//builder.Services.AddDbContext<TareaContext>(item => item.UseInMemoryDatabase("TareasDB"));
 
-builder.Services.AddDbContext<TareaContext>(item => item.UseInMemoryDatabase("TareasDB"));
+// Configuracion de base de datos postgres
+builder.Services.AddNpgsql<TareaContext>(builder.Configuration.GetConnectionString("conexionTareas"));
+
 
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/dbconexion", async ([FromServices] TareaContext dbcontext) =>
@@ -17,4 +23,5 @@ app.MapGet("/dbconexion", async ([FromServices] TareaContext dbcontext) =>
 });
 
 
+app.Run();
 app.Run();
